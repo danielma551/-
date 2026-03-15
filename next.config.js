8fcb2e7bd41ca332347e47f4eb549406a76a7fe3
Reading Website User <user@reading-website.local>
2026-03-15 21:24:05 +0800

    v21

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['epub2', '@napi-rs/canvas', 'tesseract.js', 'pdfjs-dist'],
   },
+  outputFileTracingIncludes: {
+    '/api/upload': [
+      './node_modules/pdfjs-dist/**/*',
+      './node_modules/@napi-rs/canvas/**/*',
+    ],
+  },
 }
 
 module.exports = nextConfig
